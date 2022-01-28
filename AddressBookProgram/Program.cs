7b61caeec87173da12a63e4c416fe70f44cc1ee5
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-28 21:41:31 +0530

    [Raj] Added:- The code for creating a contact details

--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -8,10 +8,17 @@ namespace AddressBookProgram
 {
     public class Program
     {
+        //Initializing variable
+        public static int count=0;
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Program");
             Console.ReadLine();
+
+            //Creating a contact with person details(UC1)
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContactDetails("Raj", "Verma", "Ghansoli", "NaviMumbai", "Maharashtra", 401546, 9517534567, "abc123@gmail.com");
+            addressBook.ViewContact();
         }
     }
 }
